4f72e1e9fa35945533e8593f813edf45b7a9874f
iwebnal <iwebnal@gmail.com>
2017-02-28 14:54:39 +0300

    a

--- app/includes/footer.php
+++ app/includes/footer.php
@@ -1,9 +1,50 @@
-<footer class="">
-	<div class="container-fluid">
+<footer class="ah-footer">
+	<div class="container">
     	<div class="row">
-			<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
-			
+			<div class="col-lg-3 col-md-3 col-sm-3 col-xs-12">
+				<div class="ah-footer-wrapp">
+					<a href="#" class="ah-footer-link">О компании</a>
+					<a href="#" class="ah-footer-link">Доставка и оплата</a>
+				</div>
 			</div>
+			<div class="col-lg-2 col-md-2 col-sm-2 col-xs-12">
+				<div class="ah-footer-wrapp">
+					<a href="#" class="ah-footer-link">Контакты</a>
+					<a href="#" class="ah-footer-link">Публичная оферта</a>
+				</div>
+			</div>
+			<div class="col-lg-4 col-md-4 col-sm-4 col-xs-12">
+				<div class="ah-footer-wrapp ah-footwrapp11">
+					<a href="#" class="ah-footer-link">Условия возврата и обмена</a>
+					<a href="#" class="ah-footer-link">Карта сайта</a>
+				</div>
+			</div>
+			<div class="col-lg-3 col-md-3 col-sm-3 col-xs-12">
+				<div class="ah-footer-wrapp1">
+					<a href="tel:+78004460320" class="ah-footer-tel">+7 800 446-03-20</a>
+					<a href="#" class="ah-footer-link1">Заказать обратный звонок</a>
+					<a href="mailto:info@fanatoy.ru" class="ah-footer-mail">info@fanatoy.ru</a>
+				</div>
+			</div>
+			<div class="clearfix"></div>
+			<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+				<div class="ah-footer-socbox">
+					<a href="#" class="ah-footsocbox-link"></a>
+					<a href="#" class="ah-footsocbox-link1"></a>
+				</div>
+			</div>	
 		</div>
 	</div>
 </footer>
+<section class="ah-footer-bottom">
+    <div class="container">
+    	<div class="row">
+			<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+				<p class="ah-footbottom-text">© 2017 fanatot.ru. All rights reserved. Все права защищены. Полное или частичное копирование материалов запрещено. При согласованном использовании материалов сайта необходима гиперссылка на ресурс.</p>
+				<a href="/" class="ah-footbottom-logo">
+					<img src="img/foot-logo.png" alt="foot-logo">
+				</a>
+			</div>
+		</div>
+	</div>
+</section>
