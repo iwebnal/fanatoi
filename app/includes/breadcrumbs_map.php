5b61f24ce85d93f052f265834ecfa5dc292db45e
iwebnal <iwebnal@gmail.com>
2017-03-03 18:06:04 +0300

    map and news

---
+++ app/includes/breadcrumbs_map.php
@@ -0,0 +1,13 @@
+<section class="az-breadcrumbs hidden-xs hidden-sm hidden-md">
+	<div class="container">
+		<div class="row">
+			<div class="col-md-12">
+				<ul>
+					<li><a href="#">Главная</a></li>
+					<li><a href="#">Каталог</a></li>
+					<li><span>Игрушка</span></li>
+				</ul>
+			</div>
+		</div>
+	</div>
+</section>
---
+++ app/map.php
@@ -0,0 +1,199 @@
+<!DOCTYPE html>
+<html lang="ru">
+
+<head>
+
+	<meta charset="utf-8">
+
+	<title>Card_Product</title>
+	<meta name="description" content="">
+
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+
+	<meta property="og:image" content="path/to/image.jpg">
+	<link rel="shortcut icon" href="img/favicon/favicon.ico" type="image/x-icon">
+	<link rel="apple-touch-icon" href="img/favicon/apple-touch-icon.png">
+	<link rel="apple-touch-icon" sizes="72x72" href="img/favicon/apple-touch-icon-72x72.png">
+	<link rel="apple-touch-icon" sizes="114x114" href="img/favicon/apple-touch-icon-114x114.png">
+
+	
+	<link rel="stylesheet" href="css/main.min.css">
+
+
+	<!-- Chrome, Firefox OS and Opera -->
+	<meta name="theme-color" content="#000">
+	<!-- Windows Phone -->
+	<meta name="msapplication-navbutton-color" content="#000">
+	<!-- iOS Safari -->
+	<meta name="apple-mobile-web-app-status-bar-style" content="#000">
+	<script src='https://www.google.com/recaptcha/api.js'></script>
+</head>
+
+<body>
+	<?php require_once('includes/header.php'); ?>
+	<?php require_once('includes/breadcrumbs_map.php'); ?>
+	<div class="clearfix"></div>
+
+	<section class="ah-card-product">
+	    <div class="container">
+	    	<div class="row">
+				<div class="col-lg-3 col-md-3 col-sm-4 hidden-xs">
+					<ul class="ah-cardproduct-list">
+						<li><a href="#">Доставка и оплата</a></li>
+						<li><a href="#">Условия возврата и обмена</a></li>
+						<li><a href="#">Публичная оферта</a></li>
+						<li><a href="#">О компании</a></li>
+						<li><a href="#">Контакты</a></li>
+					</ul>
+					<span class="az-title-wrap"><span class="az-title ah-title">товары со скидкой</span></span>
+					<div class="ah-sidebarbox">
+						<div class="ah-sidebarbox-item">
+							<div class="ah-sidebarbox-left">
+								<div class="ah-sidebarbox-imgbox">
+									<img src="img/ah-sidebaricon.jpg" alt="sidebaricon">
+								</div>
+								<span class="ah-salebox">Скидка: 30%</span>
+							</div>
+							<div class="ah-sidebarbox-right">
+								<span class="ah-sidebar-title">Фигурка Железный голем</span>
+								<span class="ah-sidebar-subtitle">Майнкрафт, набор</span>
+								<span class="ah-sidebar-price1">2 800<span>руб.</span></span>
+								<span class="ah-sidebar-price2">1 800<span>руб.</span></span>
+							</div>
+						</div>
+						<div class="ah-sidebarbox-item">
+							<div class="ah-sidebarbox-left">
+								<div class="ah-sidebarbox-imgbox">
+									<img src="img/ah-sidebaricon.jpg" alt="sidebaricon">
+								</div>
+								<span class="ah-salebox">Скидка: 30%</span>
+							</div>
+							<div class="ah-sidebarbox-right">
+								<span class="ah-sidebar-title">Фигурка Железный голем</span>
+								<span class="ah-sidebar-subtitle">Майнкрафт, набор</span>
+								<span class="ah-sidebar-price1">2 800<span>руб.</span></span>
+								<span class="ah-sidebar-price2">1 800<span>руб.</span></span>
+							</div>
+						</div>
+						<div class="ah-sidebarbox-item">
+							<div class="ah-sidebarbox-left">
+								<div class="ah-sidebarbox-imgbox">
+									<img src="img/ah-sidebaricon.jpg" alt="sidebaricon">
+								</div>
+								<span class="ah-salebox">Скидка: 30%</span>
+							</div>
+							<div class="ah-sidebarbox-right">
+								<span class="ah-sidebar-title">Фигурка Железный голем</span>
+								<span class="ah-sidebar-subtitle">Майнкрафт, набор</span>
+								<span class="ah-sidebar-price1">2 800<span>руб.</span></span>
+								<span class="ah-sidebar-price2">1 800<span>руб.</span></span>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="col-lg-9 col-md-9 col-sm-8 col-xs-12">
+					<div class="row">
+						<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+							<div class="ah-cardproduct-topwrapp">
+								<span class="ah-cardproduct-title">ООО “Игрушки Майнкрафт”</span>
+								<span class="ah-cardproduct-subtitle">Интернет магазин игрушек Майнкрафт с доставкой по всей России</span>
+							</div>
+						</div>
+						<div class="clearfix"></div>
+						<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+							<span class="ah-map-title">Склад в Москве</span>
+						</div>
+						<div class="clearfix"></div>
+						<div class="col-lg-8 col-md-8 col-sm-12 col-xs-12">
+							<div class="ah-map1-wrapp">
+								<iframe src="https://api-maps.yandex.ru/frame/v1/-/CZxPu4J8" frameborder="0"></iframe>
+							</div>
+						</div>
+						<div class="col-lg-4 col-md-4 hidden-sm col-xs-12">
+							<div class="ah-map-contactbox">
+								<span class="ah-map-subtitle">Адрес:</span>
+								<span class="ah-map-city">446999 Москва,</span>
+								<span class="ah-map-street">Садовое кольцо д. 45</span>
+								<span class="ah-map-house">офис.: 499</span>
+								<span class="ah-map-mail">Email: info@mine.ru</span>
+							</div>
+						</div>
+						<div class="clearfix"></div>
+						<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+							<span class="ah-map-title ah-map-title1">Склад в Екатеринбурге</span>
+						</div>
+						<div class="clearfix"></div>
+						<div class="col-lg-8 col-md-8 col-sm-12 col-xs-12">
+							<div class="ah-map1-wrapp">
+								<iframe src="https://api-maps.yandex.ru/frame/v1/-/CZxPmGL~" frameborder="0"></iframe>
+							</div>
+						</div>
+						<div class="col-lg-4 col-md-4 hidden-sm col-xs-12">
+							<div class="ah-map-contactbox">
+								<span class="ah-map-subtitle">Адрес:</span>
+								<span class="ah-map-city">446999 Москва,</span>
+								<span class="ah-map-street">Садовое кольцо д. 45</span>
+								<span class="ah-map-house">офис.: 499</span>
+								<span class="ah-map-mail">Email: info@mine.ru</span>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+	<div class="clearfix"></div>
+	<?php require_once('includes/popular_map.php'); ?>
+	<?php require_once('includes/vkbox_map.php'); ?>
+	<?php require_once('includes/footer.php'); ?>
+
+		<div class="popup zoom-anim-dialog mfp-hide" id="call-back">
+			<div class="form">
+				<span class="popup-title">Заказать обратный звонок</span>
+				<div class="form-item error">
+					<label for="uname">Ваше имя:</label>
+					<input type="text" name="uname" id="uname" placeholder="Введите имя:">
+				</div>
+				<div class="form-item">
+					<label for="email">Ваш e-mail:</label>
+					<input type="text" name="email" id="email" class="email" placeholder="Введите ваш e-mail:">
+				</div>
+				<div class="form-item">
+					<label for="tel">Ваш телефон:</label>
+					<input type="text" name="tel" id="tel" class="phone" placeholder="+7 (---) --- -- --">
+				</div>
+				<div class="form-item">
+					<label for="message">Ваше сообщение:</label>
+					<textarea name="message" id="message" placeholder=""></textarea>
+				</div>
+				<div class="form-item">
+					<label for="az-select">Выпадшка:</label>
+					<select class="selectpicker" data-style="az-btn" data-width="100%" name="select" id="az-select">
+					  <option>Распродажа</option>
+					  <option>Мягкие игрушки</option>
+					  <option>Оружие</option>
+					</select>
+				</div>
+				<div class="form-item">
+					<label for="az-checkbox">Чекбокс:</label><br>
+					<span class="az-checkbox">
+						<input type="checkbox" name="az-checkbox" id="az-checkbox">
+						<label for="az-checkbox"></label>
+					</span>
+				</div>
+				<div class="az-captcha-wrap">
+					<div class="az-captcha">
+						<div class="g-recaptcha" data-sitekey="6LflIhcUAAAAAKNbZ6X5Go4fEdFLd9_9Fsc4bswE"></div>
+					</div>
+				</div>
+				<div class="form-item az-center">
+					<button type="submit">Перезвоните мне</button>
+				</div>
+			</div>
+		</div>
+			
+	<script src="js/scripts.min.js"></script>
+
+</body>
+</html>
